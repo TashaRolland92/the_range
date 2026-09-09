9ff9c656be59ba305f16f252a9268b050ccb44be
TashaRolland92 <tasharolland@hotmail.co.uk>
2026-09-09 18:02:06 +0100

    loading states for screen readers

--- react_app/src/components/Spinner.jsx
+++ react_app/src/components/Spinner.jsx
@@ -1,6 +1,6 @@
 function Spinner() {
     return (
-        <div className="spinner_wrapper">
+        <div className="spinner_wrapper" role="status" aria-live="polite">
             <div className="spinner"></div>
             <p>Loading products...</p>
         </div>
